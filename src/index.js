e46d494bbf29154e0d8c83128f7ad71cb8144d73
Aakash Sharma <aakash.sharma@bcstechnology.com.au>
2022-02-10 21:46:43 +0530

    done changes in navbar

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import "jquery/dist/jquery.min.js"
+import "jquery/dist/jquery.min.js";
+import 'bootstrap/dist/js/bootstrap.bundle.min'
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+
 
 
 ReactDOM.render(
