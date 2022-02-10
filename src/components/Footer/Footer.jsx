b1cdce421eee78b335352cc80f1b4eb56f5c9f90
Aakash Sharma <aakash.sharma@bcstechnology.com.au>
2022-02-10 21:09:45 +0530

    done changes in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,13 +12,13 @@ const Footer = () => {
           <h6 className='text-center'>Software Developer</h6>
         </Fade>
         
-        <div className='social-icons d-flex justify-content-center mt-5'>
+        <div className='social-icons mt-5 d-flex justify-content-center'>
           <Bounce bottom cascade big duration={2000}>
             <ul className='list-unstyled'>
-              <li><a className='float-start px-3 fs-3' href='https://www.facebook.com/profile.php?id=100005963888429 '><FaFacebook/></a></li>
-              <li><a className='float-start px-3 fs-3' href='https://github.com/aakash1003'><FaGithub/></a></li>
-              <li><a className='float-start px-3 fs-3' href='https://www.linkedin.com/in/aakash-sh1/'><FaLinkedin/></a></li>
-              <li><a className='float-start px-3 fs-3' href='https://twitter.com/Aakash_Sh1'><FaTwitter/></a></li>
+              <li className='d-inline-block'><a className='text-secondary  px-3 fs-3' href='https://www.facebook.com/profile.php?id=100005963888429 '><FaFacebook/></a></li>
+              <li className='d-inline-block'><a className='text-secondary  px-3 fs-3' href='https://github.com/aakash1003'><FaGithub/></a></li>
+              <li className='d-inline-block'><a className='text-secondary  px-3 fs-3' href='https://www.linkedin.com/in/aakash-sh1/'><FaLinkedin/></a></li>
+              <li className='d-inline-block'><a className='text-secondary  px-3 fs-3' href='https://twitter.com/Aakash_Sh1'><FaTwitter/></a></li>
             </ul>
           </Bounce>
           
